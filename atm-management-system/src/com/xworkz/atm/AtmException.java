418923c784ce62c40cb712d0d6cbf393b3d821f6
ShilpaShahapur <shilpashahapur.xworkz@gmail.com>
2022-08-05 09:08:44 +0530

    committing file

---
+++ atm-management-system/src/com/xworkz/atm/AtmException.java
@@ -0,0 +1,10 @@
+package com.xworkz.atm;
+
+public class AtmException  extends RuntimeException{
+	
+	@Override
+	public String toString() {
+		return "You have reached maximum amount please select valid amount";
+	}
+
+}
